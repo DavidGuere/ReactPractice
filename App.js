c78235f93e99de94546e082139a752e62caafa83
GDavid <davidgallardoguere@gmail.com>
2020-12-09 15:45:49 +0100

    app.js and css for the files

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from "react";
-// import Error_Examples from "./tutorial/1-useState/setup/1-error-example";
-import Use_State_Example from "./tutorial/1-useState/setup/2-useState-basics";
+import Setup from "Path";
+
 function App() {
   return (
     <div className="container">
-      {/* <Error_Examples /> */}
-      <Use_State_Example />
+      <Setup />
     </div>
   );
 }
